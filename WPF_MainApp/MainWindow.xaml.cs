c3020ec0a5f39b90d426d869ac0104f791ec6114
Hanna Kristensson <hanna.kristensson@hotmail.com>
2025-01-08 11:09:55 +0100

    First attempt of frontpage design

--- WPF_MainApp/MainWindow.xaml.cs
+++ WPF_MainApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Windows;
+using System.Windows.Input;
 using WPF_MainApp.ViewModels;
 
 
@@ -11,5 +12,18 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         DataContext = viewModel;
+    }
+
+    private void TopBar_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            DragMove();
+        }
+    }
+
+    private void ExitButton_Click(object sender, RoutedEventArgs e)
+    {
+        Environment.Exit(0);
     }
 }
