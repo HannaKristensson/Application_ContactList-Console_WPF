51c92098366ec42f41a6b4ed6fa1fd2d736fe1a4
Hanna Kristensson <hanna.kristensson@hotmail.com>
2025-01-08 11:57:41 +0100

    First attempt AddContact design

--- WPF_MainApp/App.xaml.cs
+++ WPF_MainApp/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿
+using Buisness.Interfaces;
+using Buisness.Services;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Windows;
@@ -17,12 +19,17 @@ public partial class App : Application
         _host = Host.CreateDefaultBuilder()
             .ConfigureServices(services =>
             {
+                services.AddTransient<IContactService, ContactService>();
+                services.AddTransient<IFileService, FileService>();
+
                 services.AddSingleton<MainViewModel>();
                 services.AddSingleton<MainWindow>();
+
                 services.AddSingleton<HomeView>();
                 services.AddSingleton<HomeViewModel>();
 
                 services.AddTransient<ContactsViewModel>();
+                services.AddTransient<ContactsView>();
 
                 services.AddTransient<AddContactsViewModel>();
                 services.AddTransient<AddContactView>();
